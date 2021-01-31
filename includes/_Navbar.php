203ce0ab7c8d25ebc4d1ec1326747c407f7e5354
josipfrljic94 <josipfrljic77@gmail.com>
2021-01-31 01:36:24 +0100

    navbar

---
+++ includes/_Navbar.php
@@ -0,0 +1,38 @@
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Brand</a>
+    <button class="navbar-toggler border-0 shadow-sm" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon border-0"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="index.php">Products</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="/About.php">Categories</a>
+        </li>
+
+
+        <li class="nav-item">
+          <a class="nav-link" href="cart.php">
+            <svg class="m-0 mb-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 1a2.5 2.5 0 0 0-2.5 2.5V4h5v-.5A2.5 2.5 0 0 0 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V5H2z"/>
+                <path fill-rule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"/>
+            
+            </svg>
+            <span class="badge bg-danger m-0   p-1 text-left " style="font-size:0.6rem;">15</span> 
+          </a>
+         
+        </li>
+      
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+    
+  </div>
+</nav>
